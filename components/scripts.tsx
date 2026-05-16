f573572ef558606f8abe161c3346daffcf72384f
rosencoding <krz.cichy@gmail.com>
2026-05-16 22:25:40 +0200

    Add Simple Analytics script

--- components/scripts.tsx
+++ components/scripts.tsx
@@ -4,13 +4,21 @@ export default function Scripts() {
     const domain = "scripts.howbrowserswork.com";
 
     return (
-        <Script
-            id="pianjs"
-            src={`https://${domain}/static/files/pa.js`}
-            data-hit-endpoint={`https://${domain}/p/pv`}
-            data-event-endpoint={`https://${domain}/p/e`}
-            data-session-endpoint={`https://${domain}/p/s`}
-            strategy="afterInteractive"
-        />
+        <>
+            <Script
+                id="pianjs"
+                src={`https://${domain}/static/files/pa.js`}
+                data-hit-endpoint={`https://${domain}/p/pv`}
+                data-event-endpoint={`https://${domain}/p/e`}
+                data-session-endpoint={`https://${domain}/p/s`}
+                strategy="afterInteractive"
+            />
+            {/* 100% privacy-first analytics */}
+            <Script
+                id="simple-analytics"
+                src="https://scripts.simpleanalyticscdn.com/latest.js"
+                strategy="afterInteractive"
+            />
+        </>
     );
 }
